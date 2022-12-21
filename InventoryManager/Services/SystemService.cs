4b1d2e42cf516065f2d467d18a306a1ce1aa2e02
ZM <zanamziry720@gmail.com>
2022-12-21 23:30:40 +0300

    auto start python

--- InventoryManager/Services/SystemService.cs
+++ InventoryManager/Services/SystemService.cs
@@ -10,6 +10,8 @@ public class SystemService : ISystemService
     {
     }
 
+    readonly string cmdArgs = "./dxn_api/manage.py 127.0.0.1 80";
+    Process _process;
     public void OpenInWebBrowser(string url)
     {
         // For more info see https://github.com/dotnet/corefx/issues/10361
@@ -19,5 +21,31 @@ public class SystemService : ISystemService
             UseShellExecute = true
         };
         Process.Start(psi);
+    }
+
+    public void StartServer()
+    {
+        if (_process != null)
+            return;
+        _process = new Process();
+        _process.StartInfo = new ProcessStartInfo(@"C:\Python27\python.exe", cmdArgs)
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+        _process.Start();
+        
+        string output = _process.StandardOutput.ReadToEnd();
+        p.WaitForExit();
+        Console.WriteLine(output);
+        Console.ReadLine();
+    }
+
+    public void StopServer()
+    {
+        if (_process == null)
+            return;
+        _process.Kill();
     }
 }
